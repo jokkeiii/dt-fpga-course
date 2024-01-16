370af3424dcefb54e242ae4bc77405967d52a331
jokkeiii <54310059+jokkeiii@users.noreply.github.com>
2024-01-16 13:05:19 +0200

    AND-operation

--- dt-fpga-vivado-init.srcs/sources_1/new/TOP.v
+++ dt-fpga-vivado-init.srcs/sources_1/new/TOP.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company:
+// Engineer:
 // 
 // Create Date: 01/16/2024 10:40:14 AM
 // Design Name: 
@@ -23,13 +23,13 @@
 module TOP(
     input wire [3:0] sw,
     output wire led0,
-    output wire led1,
-    output wire led2,
-    output wire led3
+    // output wire led1,
+    output wire led2
+    // output wire led3
     );
 
-    assign led0 = sw[0];
-    assign led1 = sw[1];
-    assign led2 = sw[2];
-    assign led3 = sw[3];    
+    assign led0 = sw[0] & sw[1];
+    // assign led1 = sw[1];
+    assign led2 = sw[2] & sw[3];
+    // assign led3 = sw[3];    
 endmodule
